f555c226e9f1a49ab00fb4ad10c2d32d0265fe89
Justin Hui <wingchung@justinhui.co.uk>
2014-12-16 21:44:48 +0000

    Added authentication and boostrap template

--- familyroot/urls.py
+++ familyroot/urls.py
@@ -5,11 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'familyroot.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'familyroot.views.index', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^about/$', 'familyroot.views.about', name='about'),
 
     #user auth urls
     url(r'^accounts/login/$', 'familyroot.views.login'),
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', 'familyroot.views.logout'),
     url(r'^accounts/logged_in/$', 'familyroot.views.logged_in'),
     url(r'^accounts/invalid/$', 'familyroot.views.invalid_login'),
+
 )
