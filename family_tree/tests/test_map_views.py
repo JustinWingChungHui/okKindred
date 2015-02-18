af005fd785de0cd39c9adbdb24283ccefb83d4be
Justin Hui <wingchung@justinhui.co.uk>
2015-02-18 01:18:59 +0000

    Correct commas on map view when in Polish language

--- family_tree/tests/test_map_views.py
+++ family_tree/tests/test_map_views.py
@@ -15,7 +15,7 @@ class TestMapViews(TestCase):
 
 
         self.user = User.objects.create_user(email='adam_lambert@queenonline.com', password='sexy boy', name='Adam Lambert', family_id=self.family.id)
-        self.user.save()
+
 
         self.person1 = Person(name='Adam Lambert', gender='M', user_id = self.user.id, email='adam_lambert@queenonline.com'
                                     , family_id=self.family.id, latitude = 39.768, longitude = -86.158, address = 'Indianapolis, Indiana')
@@ -69,3 +69,32 @@ class TestMapViews(TestCase):
 
         self.assertTrue(b'center: new google.maps.LatLng(54.574,-1.235)' in response.content)
 
+
+    def test_map_view_loads_in_polish(self):
+        '''
+        This is to check a bug where coordinates were formatted to have commas instead of decimal points
+        when language set to Polish
+        '''
+
+        #Create a Polish user
+        user = User.objects.create_user(email='szajka@nowahuta.pl', password='nowa huta', name='Szajka', family_id=self.family.id, language='pl')
+        self.client.login(email='szajka@nowahuta.pl', password='nowa huta')
+        Person.objects.create(name='Szajka', gender='M', user_id = user.id, email='szajka@nowahuta.pl', family_id=self.family.id, language='pl')
+        from django.utils import translation
+        translation.activate('pl')
+
+        response = self.client.get('/map={0}/'.format(self.person2.id))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'family_tree/family_map.html')
+
+        self.assertTrue(b'Paul Rodgers' in response.content)
+
+        self.assertTrue(b'x.push(54.574)' in response.content)
+        self.assertTrue(b'y.push(-1.235)' in response.content)
+
+        self.assertTrue(b'Adam Lambert' in response.content)
+        self.assertTrue(b'x.push(39.768)' in response.content)
+        self.assertTrue(b'y.push(-86.158)' in response.content)
+
+        self.assertTrue(b'center: new google.maps.LatLng(54.574,-1.235)' in response.content)
