02153b6e4ef7e0fbf95c2a514c35648fe41e420f
Justin Hui <wingchung@justinhui.co.uk>
2017-02-20 17:31:26 +0000

    Fixed lockout issue and added modified carousel

--- custom_user/decorators.py
+++ custom_user/decorators.py
@@ -16,3 +16,16 @@ def set_language(func):
 
 
     return inner
+
+
+def set_pythonanywhere_external_ip(func):
+    '''
+    Ensures that the external IP address is set not the PythonAnywhee load balancer
+    '''
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        request.META['REMOTE_ADDR'] = request.META.get('HTTP_X_REAL_IP')
+
+        return func(request=request, *args, **kwargs)
+
+    return inner
