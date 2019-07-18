c7a5a50de2221e2088fe96cde3b0c69dbe0936d3
Justin Hui <wingchung@justinhui.co.uk>
2019-07-18 12:12:36 +0000

    Added password changed email

--- auth_api/signals.py
+++ auth_api/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.template.loader import get_template
 from django.utils import translation
 
-from django_rest_passwordreset.signals import reset_password_token_created
+from django_rest_passwordreset.signals import reset_password_token_created, post_password_reset
 
 from axes.signals import user_locked_out
 
@@ -75,3 +75,35 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         fail_silently=False,
         html_message=content_html)
 
+
+
+@receiver(post_password_reset)
+def post_password_reset(user, *args, **kwargs):
+    """
+    Called once user has changed password.  Sends email to confirm change
+    """
+    language = user.language
+
+    translation.activate(language)
+
+    subject = translation.ugettext('ok!Kindred Password Successfully Updated')
+
+    content = get_template('auth_api/password_changed_plain.html').render(
+                {
+                    'language': language,
+                    'domain' : settings.DOMAIN
+                })
+
+    content_html = get_template('auth_api/password_changed.html').render(
+                {
+                    'language': language,
+                    'domain' : settings.DOMAIN
+                })
+
+    send_mail(
+        subject,
+        content,
+        'info@okkindred.com',
+        [user.email],
+        fail_silently=False,
+        html_message=content_html)
