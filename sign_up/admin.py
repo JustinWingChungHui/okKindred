9e4cf1e0aeb885c681e75dc0ceb6a4e38949e225
Justin Hui <wingchung@justinhui.co.uk>
2019-09-20 15:15:43 +0000

    Added ip address to admin sign up view

--- sign_up/admin.py
+++ sign_up/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from sign_up.models import SignUp
 
 class SignUpAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('email_address','ip_address','name',)
 
 admin.site.register(SignUp,SignUpAdmin)
