82b3e92cb77881ae59fe792f567315e2c237c97a
Justin Hui <wingchung@justinhui.co.uk>
2019-10-01 16:21:46 +0000

    Updated scheduled jobs to reflect new virtualenv

--- schedule/create_daily_emails.sh
+++ schedule/create_daily_emails.sh
@@ -1,2 +1,2 @@
-source ~/.virtualenvs/python3.7/bin/activate
+source ~/.virtualenvs/api.okkindred/bin/activate
 python ~/okkindred/manage.py create_daily_emails
--- schedule/expire_invitation_emails.sh
+++ schedule/expire_invitation_emails.sh
@@ -1,4 +1,4 @@
-source ~/.virtualenvs/python3.7/bin/activate
+source ~/.virtualenvs/api.okkindred/bin/activate
 python ~/okkindred/manage.py expire_invitations
 python ~/okkindred/manage.py expire_sign_ups
 
