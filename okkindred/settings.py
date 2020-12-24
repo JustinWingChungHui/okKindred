e787a2141b12639365f7e400f515ee7e60b38779
Justin Wing Chung Hui <wingchung@justinhui.co.uk>
2020-12-24 14:47:08 +0000

    Updated to latest python package

--- okkindred/settings.py
+++ okkindred/settings.py
@@ -223,6 +223,7 @@ TEMPLATES = [
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
+                'django.template.context_processors.request',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
             ],
